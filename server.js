8ab47f1f7da5d75eaab9d15b71277ce226fd05ae
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-08-28 08:41:45 +0530

    insert and update method

--- server.js
+++ server.js
@@ -2,19 +2,55 @@ import express from "express";
 import connectToMongo from "./dataBase.js";
 // instance of express
 const app = express();
+app.use(express.json());
 
-app.get("/", async (req, res) => {
+// //get req
+// app.get("/", async (req, res) => {
+//   try {
+//     const db = await connectToMongo();
+//     const collection = db.collection("user");
+//     const resdata = await collection.find().toArray();
+//     res.json(resdata);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({
+//       success: false,
+//       error,
+//     });
+//   }
+// });
+
+// //post req
+// app.post("/create", async (req, res) => {
+//   try {
+//     const db = await connectToMongo(); //db is our data base in mongo db like student
+//     const collection = db.collection("user"); //collection
+
+//     const data = req.body;
+//     console.log(data);
+
+//     //insert doc
+//     const result = collection.insertOne(data); //get data from client
+//     res.json(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+//update
+app.put("/update/:name", async (req, res) => {
   try {
-    const db = await connectToMongo();
-    const collection = db.collection("user");
-    const resdata = await collection.find().toArray();
-    res.json(resdata);
+    const db = await connectToMongo(); //db is our data base in mongo db like student
+    const collection = db.collection("user"); //collection
+    console.log(req.body);
+    console.log(req.params);
+    const result = collection.updateOne(
+      { name: req.params.name },
+      { $set: req.body }
+    );
+    res.json("updated");
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      success: false,
-      error,
-    });
   }
 });
 
